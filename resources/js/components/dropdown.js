141d3e2db7ba979c366152671bb766e8a57ca1a7
Ongki Herlambang <meepomodeon@gmail.com>
2019-12-30 16:06:44 +0700

    refine table info

--- resources/js/components/dropdown.js
+++ resources/js/components/dropdown.js
@@ -16,13 +16,13 @@ export function generateDropdown({ label, id, name, value, initial, placeholder 
   let markup = `
     <fieldset class="input">
       <label for="${id}" class="input-label">${label}</label>
+      <span class="input-arrow">
+        <svg viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false" style="height: 16px; width: 16px; display: block; fill: currentColor;"><path d="m16.29 4.3a1 1 0 1 1 1.41 1.42l-8 8a1 1 0 0 1 -1.41 0l-8-8a1 1 0 1 1 1.41-1.42l7.29 7.29z" fill-rule="evenodd"></path></svg>
+      </span>
       <select class="input-field input-dropdown" id="${id}" name="${name.trim()}" data-initial="${initial}" value="${value}">
         <option disabled>${placeholder}</option>
         ${transformedOptions}
       </select>
-      <span class="input-arrow">
-        <svg viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false" style="height: 16px; width: 16px; display: block; fill: currentColor;"><path d="m16.29 4.3a1 1 0 1 1 1.41 1.42l-8 8a1 1 0 0 1 -1.41 0l-8-8a1 1 0 1 1 1.41-1.42l7.29 7.29z" fill-rule="evenodd"></path></svg>
-      </span>
     </fieldset>
   `
 
--- resources/js/datatables/users.js
+++ resources/js/datatables/users.js
@@ -61,7 +61,7 @@ let usersTable = $('#users-table').DataTable({
     processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>',
   },
   pageLength: 25,
-  dom: 'ilfrt<"#tableFooter" p>',
+  dom: '<"table-footer-info"  i <".separator"> l>frt<"#tableFooter.datatable-footer" p>',
 })
 
 $(
