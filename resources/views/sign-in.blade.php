90be0476e6948f58e0768c3c87c2ed742bb4603c
Ongki Herlambang <meepomodeon@gmail.com>
2020-01-03 11:31:06 +0700

    refine quilld, refine sign-in page

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -5,27 +5,32 @@
 @section('content')
 
 <div class="background-wrapper">
-  <div class="login-wrapper">
-    <div class="container-narrow login-container">
-      <header class="login-header">
-        <h1 class="display3">Sign in</h1>
-        <p class="large">Please sign in to your account to continue.</p>
-      </header>
-      <form method="POST" enctype="multipart/form-data" action="/sign-in" class="login-form">
-        {{ csrf_field() }}
-        <fieldset class="input">
-          <label class="input-label" for="email">Email</label>
-          <input autocomplete="false" autofocus class="input-field" type="email" id="email" name="email">
-        </fieldset>
-        <fieldset class="input" style="margin-bottom: 24px">
-          <label class="input-label" for="password">Password</label>
-          <input autocomplete="false" class="input-field" type="password" id="password" name="password">
-        </fieldset>
-        <input class="button primary button--large" type="submit" value="Sign in" />
-        {{-- <a class="button primary-outline button--large stretch" href="/">Go to Home</a> --}}
-      </form>
+  <div class="login-container">
+    <div class="login-brand">
+      <a href="/">
+        @include('icons.pmm')
+      </a>
     </div>
+    <form method="POST" enctype="multipart/form-data" action="/sign-in" class="login-form">
+      <header class="login-header">
+        <h1 class="heading1">Sign in</h1>
+        <p class="medium">Please sign in to your account to continue.</p>
+      </header>
+      {{ csrf_field() }}
+      <fieldset class="input">
+        <label class="input-label" for="email">Email</label>
+        <input autocomplete="false" autofocus class="input-field" type="email" id="email" name="email">
+      </fieldset>
+      <fieldset class="input" style="margin-bottom: 24px">
+        <label class="input-label" for="password">Password</label>
+        <input autocomplete="false" class="input-field" type="password" id="password" name="password">
+      </fieldset>
+      <input class="button primary button--large" type="submit" value="Sign in" />
+    </form>
+    <footer class="stack--l">
+      <a style="display: inline-flex;" class="button ghost" href="/forgot-password">Forgot Password</a>
+    </footer>
   </div>
-  </div>
+</div>
 
 @endsection
