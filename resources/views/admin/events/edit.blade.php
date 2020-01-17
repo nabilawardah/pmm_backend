a7b981939218ad061275ea273f6082e30ba6ee3c
Ongki Herlambang <meepomodeon@gmail.com>
2020-01-18 00:22:30 +0700

    step 1 add event

--- resources/views/admin/events/edit.blade.php
+++ resources/views/admin/events/edit.blade.php
@@ -6,7 +6,8 @@
 
   <style>
     .main-content {
-      padding-bottom: 96px;
+      padding-top: 0;
+      padding-bottom: 0;
     }
   </style>
 
@@ -15,6 +16,7 @@
     'action_class' => 'publish-event',
     'action_label' => 'Publish Event'
   ])
+
   @include('components.confirm-delete', [
     'message' => 'Deleted events are gone forever. Are you sure?',
     'url' => '/api/events/delete/'.$event['id']
@@ -24,17 +26,18 @@
   @component('layouts.main-content', ['width' => 'narrow'])
 
     <main class="edit-event-step step-1">
+
       <section>
+
         <header class="editor-header-wrapper">
           <h1 id="editor-title" class="editor-title" contenteditable autofocus>{{ $event['title'] ?? '' }}</h1>
         </header>
 
         <section class="editor-subtitle-wrapper" style="margin-bottom: 48px;">
-          <p id="editor-subtitle-preview" class="editor-subtitle-preview event-summary" contenteditable>{{$event['subtitle'] ?? 'Summary'}}</p>
+          <p id="editor-subtitle-preview" class="editor-subtitle-preview event-summary" contenteditable>{{$event['subtitle'] ?? ''}}</p>
         </section>
 
         <section style="margin-bottom: 48px;">
-
           <h2 class="heading3" style="margin-bottom: 32px;">Date and Time</h2>
           <div class="date-time-outer-wrapper">
             <fieldset class="input date-picker-container" style="margin-bottom: 0;">
@@ -47,7 +50,6 @@
               <input class="input-field" type="text" id="time-start" placeholder="Start">
             </fieldset>
           </div>
-
           <div class="date-time-outer-wrapper">
             <fieldset class="input date-picker-container" style="margin-bottom: 0;">
               <label class="input-label" for="date-end">End Date</label>
@@ -59,24 +61,28 @@
               <input class="input-field" type="text" id="time-end" placeholder="End">
             </fieldset>
           </div>
-
         </section>
 
-        <section>
+        <section style="margin-bottom: 48px;">
           <h2 class="heading3" style="margin-bottom: 32px;">Venue</h2>
           <fieldset class="input">
             <label for="venue-name" class="input-label">Name</label>
             <input type="text" class="input-field" id="venue-name" placeholder="Venue Name" value="{{ $event['venue']['name'] ?? '' }}">
           </fieldset>
           <fieldset class="input">
-            <label for="venue-detail" class="input-label">Name</label>
-            <input type="text" class="input-field" id="venue-detail" placeholder="Venue Name" value="{{ $event['venue']['address'] ?? '' }}">
+            <label for="venue-detail" class="input-label">Address</label>
+            <textarea style="resize: vertical;" class="input-field" name="venue-detail" id="venue-detail" rows="4">{{ $event['venue']['address'] ?? '' }}</textarea>
           </fieldset>
         </section>
 
+        <footer>
+          <button class="button primary button--large">Continue</button>
+          <button class="button ghost button--large">Cancel</button>
+        </footer>
+
       </section>
 
-      <section>
+      <section class="editor-poster-outer-wrapper">
         @isset($event['poster'])
           <div class="editor-cover-container editor-cover-container--event">
             <img class="editor-cover-image editor-cover-image--event" width="100%" data-name="{{ $event['poster'] }}" src="/media/user-1/{{ $event['poster'] }}">
@@ -128,7 +134,7 @@
 
     </main>
 
-    <input name="editor-cover" type="file" class="editor-cover">
+    <input name="editor-cover" hidden type="file" class="editor-cover">
     <input type="hidden" id="event-id" name="event-id" value="{{ $event_id }}" />
     <input type="hidden" id="user-id" name="user-id" value="{{ $user_id }}" />
     <input type="hidden" name="event-title" />
