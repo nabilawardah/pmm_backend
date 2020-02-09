1eb4d30df0c58b3ab03615ef0e856d37e0ed917c
Ongki Herlambang <meepomodeon@gmail.com>
2020-02-09 14:13:27 +0700

    update favicon, refine slick carousel

--- resources/views/layouts/web-provider.blade.php
+++ resources/views/layouts/web-provider.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="shortcut icon" type="image/png" href="{{ asset('/images/h-italics--dark.png') }}"/>
+        <link rel="shortcut icon" type="image/png" href="{{ asset('/images/logo.png') }}"/>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 
