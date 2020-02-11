b0cf199894bdb707929c9eba8f206730ca850bc8
Ongki Herlambang <meepomodeon@gmail.com>
2020-02-11 11:38:18 +0700

    refine admin navbar

--- resources/views/components/navbar-admin.blade.php
+++ resources/views/components/navbar-admin.blade.php
@@ -19,14 +19,16 @@
   ];
 
   $secondary_menus = [
-      // (object) [
-      //     'name' => 'Sign Out',
-      //     'to' => '/logout',
-      // ],
       (object) [
-          'name' => 'Back to PMM',
-          'to' => '/',
-      ],
+      'class' => '',
+      'name' => 'Profile',
+      'to' => '/profile/1',
+    ],
+    (object) [
+      'class' => 'no-pre',
+      'name' => 'Sign out',
+      'to' => '/sign-out',
+    ],
   ];
 @endphp
 
@@ -43,11 +45,25 @@
         @endcomponent
       @endforeach
     </ul>
-    <ul class="navbar-secondary inline--ll navbar-admin">
-      @foreach ($secondary_menus as $menu)
-        @component('components.menu-admin', ['name' => $menu->name, 'to' => $menu->to, 'active' => $active_page])
-        @endcomponent
-      @endforeach
+    <ul class="navbar-secondary inline--ml">
+      @component('components.menu-admin', ['name' => 'Back to PMM', 'to' => '/', 'active' => false])
+      @endcomponent
+      <li class="primary-menu-wrapper" style="padding-left: 14px; padding-right: 14px;">
+        <div class="custom-dropdown-wrapper">
+          <button class="custom-dropdown-trigger">
+            <div class="current-user-wrapper">
+              <img class="current-user-profile-photo" src="/images/users/default.png" alt="user currently login">
+            </div>
+          </button>
+          <ul class="hidden custom-dropdown-menu-wrapper popout" style="display: none;">
+            @foreach ($secondary_menus as $menu)
+              <li class="custom-dropdown-menu-item">
+                <a class="custom-dropdown-menu heading6 {{ $menu->class ?? '' }} " href="{{ $menu->to }}">{{ $menu->name }}</a>
+              </li>
+            @endforeach
+          </ul>
+        </div>
+      </li>
     </ul>
   </div>
 </nav>
